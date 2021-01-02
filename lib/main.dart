44dca7b1b14588c9ea5281e6dbd6f7090a7829bb
recepbasturk <recepbasturk@gmail.com>
2021-01-02 12:37:46 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'locator.dart';
+import 'pages/splash_page.dart';
 
+const settingsBox = 'settingsBox';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   setupLocator();
+  await Hive.initFlutter();
+  await Hive.openBox(settingsBox);
   runApp(MyApp());
 }
 
@@ -14,14 +20,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'MultiCamp Flutter Haber',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
+        primaryIconTheme: IconThemeData(color: Colors.white),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Scaffold(
-        body: Container(),
-      ),
+      home: SplashPage(),
     );
   }
 }
