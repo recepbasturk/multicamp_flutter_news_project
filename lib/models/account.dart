337c55a83c7354ef0e631967aae48ca92613b50c
recepbasturk <recepbasturk@gmail.com>
2021-01-02 12:36:59 +0300

    Update account.dart

--- lib/models/account.dart
+++ lib/models/account.dart
@@ -2,36 +2,40 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
 class Account {
-  final String accountID;
+  final String uid;
   String email;
   String fullName;
-  String profilePhoto;
+  String photoUrl;
   DateTime createdAt;
 
-  Account({@required this.accountID, @required this.email});
+  Account(
+      {@required this.uid,
+      @required this.email,
+      @required this.fullName,
+      this.photoUrl,
+      this.createdAt});
 
   Map<String, dynamic> toMap() {
     return {
-      "accountID": accountID,
+      "uid": uid,
       "email": email,
       "fullName": fullName,
-      "profilePhoto": profilePhoto ?? "images/avatar.jpg",
+      "photoUrl": photoUrl ?? "assets/images/avatar.jpg",
       "createdAt": createdAt ?? FieldValue.serverTimestamp(),
     };
   }
 
   Account.fromMap(Map<String, dynamic> map)
-      : accountID = map["accountID"],
+      : uid = map["uid"],
         email = map["email"],
         fullName = map["fullName"],
-        profilePhoto = map["profilePhoto"],
+        photoUrl = map["photoUrl"],
         createdAt = (map["createdAt"] as Timestamp).toDate();
 
-  Account.accountIDAndPhoto(
-      {@required this.accountID, @required this.profilePhoto});
+  Account.uidAndPhotoUrl({@required this.uid, @required this.photoUrl});
 
   @override
   String toString() {
-    return 'Account{accountID: $accountID, email: $email, fullName: $fullName, profilePhoto: $profilePhoto, createdAt: $createdAt}';
+    return 'Account{uid: $uid, email: $email, fullName: $fullName, photoUrl: $photoUrl, createdAt: $createdAt}';
   }
 }
